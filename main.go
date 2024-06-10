03228b7a0e8c46a5b7b10c46d3391469ab0f1695
Zagel <zmohamed6991@gmail.com>
2024-06-10 20:16:00 +0100

    Book Catalog

--- main.go
+++ main.go
@@ -1,167 +1,66 @@
 package main
 
-import (
+import 
 	"fmt"
-	//"strconv"
-)
-	
 
-func main() {
+func main () {
 
-	/* myAge := 27
-	var myName string = "Zagel"
-	
-		fmt.Printf("my name is %v\n", myName)
-		fmt.Printf("i am %d years old\n", myAge)
-
-	 var hobby string 
-	 
-	 var holiday string
-
-	 fmt.Println("Please enter your favourite hobby")
-
-	 fmt.Scan(&hobby)
-
-	 fmt.Println(hobby, "is a very cool hobby")
+	/* creating a project that will ask for book data to the user. 
+	for each book we should ask for the title, author, and publication year
+	Now, depending on the year we should say the century of the book.
 
 
 
-	 fmt.Println("where do you like to go on holiday?")
-
-	 fmt.Scan(&holiday)
-
-	 */
-
-/*
-	 var bankBalance int
-	 const housePrice int = 1900
-
-	 for { 
-
-
-	 fmt.Println("What is your bank balance?")
-	 fmt.Scan(&bankBalance)
-
-	 //fmt.Println("please enter house price:")
-	 //fmt.Scan(&housePrice)
-
-	 if  bankBalance > housePrice {
-	 fmt.Println("congratulations you can buy it")
-
-	 } else if housePrice > bankBalance {
-		fmt.Println("Insufficient funds")
-
-	 } else {
-		fmt.Println("wait")
-	 }
-
-	}
 
 	*/
-/*
-	var name string
-	var Surname string
 
-	fmt.Println("What is your name?")
-	fmt.Scan(&name)
+	var title string
+	var author string
+	var publicationYear int
 
-	fmt.Println("What is your surname?")
-	fmt.Scan(&Surname)
+	var exit bool = false 
 
-	fmt.Printf("Hi %v %v, thank you for your details", name, Surname)
-	
+	for exit == false {
 
- SIMPLE CALCULATOR
+		fmt.Println("Please enter the book title: ")
+		fmt.Scan(&title)
 
-	Create a simple calculator in Go that can perform basic arithmetic operations (addition, subtraction, multiplication, and division)
- 	based on user input. This challenge will help you practice using variables, printing and scanning user input, and 
- 	conditional statements.
+		if title == "exit" || title == "Exit" {
+			exit = true
+			continue
+		}
 
-	Requirements
+		fmt.Println("Please enter the book author: ")
+		fmt.Scan(&author)
 
-	- Prompt the user to enter two numbers.
-	- Prompt the user to choose an arithmetic operation (+, -, *, /).
-	- Perform the chosen operation on the two numbers.
-	- Print the result of the operation.
-	*/
+		fmt.Println("Please the book publication year: ")
+		fmt.Scan(&publicationYear)
+		 
+		if publicationYear != 1635 && publicationYear != 1724 && publicationYear != 1820 && publicationYear != 1920 && publicationYear != 2020 {
+			exit = true 
+			continue
+		}
 
-	var firstNumber int
-	var secondNumber int
-	var selectOperation string
+		switch publicationYear {
+		case 1635:
+			fmt.Println("Your book belongs to the 17th century.")
+		case 1724:
+			fmt.Println("Your book belongs to the 18th century.")
+		case 1820:
+			fmt.Println("Your book belongs to the 19th century")
+		case 1920:
+			fmt.Println("Your book belons to the 20th century.")
+		case 2020: 
+			fmt.Println("Your book belongs to the 21st century")
 
-	fmt.Println("Hi, I am a simple calculator")
-
-	fmt.Println("Type your first number:")
-	fmt.Scan(&firstNumber)
-
-	fmt.Println("Type your second number:")
-	fmt.Scan(&secondNumber)
-
-	fmt.Println("Choose your your operation? e.g +, -, *, /")
-	fmt.Scan(&selectOperation)
-
-	
-	// conditions 
-
-	switch selectOperation {
-		case "+":
-			fmt.Println(firstNumber + secondNumber)
-		case "-": 
-			fmt.Println(firstNumber - secondNumber)
-		case "*": 
-			fmt.Println(firstNumber * secondNumber)
-		case "/":
-			fmt.Println(firstNumber / secondNumber)
-		default:
-			fmt.Println("try again")
 		}
 
 
-	 
-
-
-
-	
+	}	
 
 
 
 
 
 
-
-
-
-
-
-
-
-		
-
-
-
-
-
-
-		
-
-
-
-		/*
-
-		var price float32 = 20.5
-
-		budget := 3000.50
-
-		var exit bool = false
-
-		isRich := true
-		var name string = "Zagel"
-
-		surname := "Mohamed"
-
-		*/
-
-
-
-	// how to print golang variable?
-	}
+}
